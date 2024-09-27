18dcc8b4a19c8bf8e2488f97b3294e368dbcfbb3
pedroedu06 <pedro.nascimento182501@gmail.com>
2024-09-27 20:08:35 -0300

    codigo completo

--- teste.c
+++ teste.c
@@ -23,41 +23,24 @@ int decimal(char high, char low) {
     return (int)strtol(hex, NULL, 16);
 }
 
-int main (){
+int main(){
 
-    int x = 1;
-    double b = 0;
-    int i;
+    double b = 10;
     char mensagem[] = "48656C6C6F2C20776F726C6421";
-
-    if (strlen(mensagem) % 2 != 0){
-        printf("tem numeros pares");
-        return 0;
-    }
+    int i = 0;
+    int x = 1;
 
     double resultado = fxb(x, b);
-    printf("resultdado funcao: %lf\n", resultado);
+    printf("resuldado da formula: %lf\n", resultado);
 
-    while(i < strlen(mensagem) && mensagem[i] != '\0' ){
-    
-        if(!((mensagem[i] >= '0' && mensagem[i] <= '9') || (mensagem[i] >= 'A' && mensagem[i] <= 'F'))){
-            printf("caracteres invalidos");
-            break;
-        }
+    while (i < strlen(mensagem) && mensagem[i] != '\0'){
 
-        int valorDecimal = decimal(mensagem[i], mensagem[i + 1]);
-
-        if (valorDecimal == 0){
-            break;
-        }
-        
-        printf("%c", valorDecimal);
-        i+=2;
+        printf("%c%c", mensagem[i], mensagem[i +1]);
+        i += 2;
         x++;
     }
-
     printf("\n");
+
 
     return 0;
 }
-
